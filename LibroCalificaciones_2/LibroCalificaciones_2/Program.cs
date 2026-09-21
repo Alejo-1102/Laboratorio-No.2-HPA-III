816b555138bb507ea5ef0aa661cf824ac4deace8
Alejo-1102 <aa.garcialedezma@gmail.com>
2026-09-21 09:40:00 -0500

    Add files via upload

---
+++ LibroCalificaciones_2/LibroCalificaciones_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibroCalificaciones_2
+{
+    public class LibroPuebaCalificaciones
+    {
+        static void Main(string[] args)
+        {
+            // Instancia del objeto MiLibroCalificaciones
+            MiLibroCalificaciones MyLibro = new MiLibroCalificaciones();
+
+            // Pide el nombre del Curso y lo recibe como entrada
+            Console.WriteLine("Por favor ingrese el nombre del curso: ");
+             
+            string nombreDelCurso = Console.ReadLine(); // Lee una línea de Texto
+            Console.WriteLine(); // Imprime en pantalla una línea en blanco
+
+            // Llama al método MostrarMensaje de MiLibroCalificaciones
+            // y pasa el nombre del curso como argumento
+            MyLibro.MostrarMensaje(nombreDelCurso);
+        }
+    }
+}
