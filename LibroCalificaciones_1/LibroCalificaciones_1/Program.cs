c19e0909bfb4e641c5417427b845b404aa1cb224
Alejo-1102 <aa.garcialedezma@gmail.com>
2026-09-21 09:39:18 -0500

    Add files via upload

---
+++ LibroCalificaciones_1/LibroCalificaciones_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LibroCalificaciones_1
+{
+    internal class PruebaLibroCalificaciones_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Instancias la clase donde SÍ creaste el método:
+            LibroCalificacion MyLibro = new LibroCalificacion();
+
+            // Ahora sí encuentra el método sin marcar error
+            MyLibro.MostrarMensaje();
+        }
+    }
+}
