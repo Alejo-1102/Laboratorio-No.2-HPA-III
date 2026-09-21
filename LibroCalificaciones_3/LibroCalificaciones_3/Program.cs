5e25ce7b5da560e37ad6dbddce2597697400ad24
Alejo-1102 <aa.garcialedezma@gmail.com>
2026-09-21 09:40:41 -0500

    Add files via upload

---
+++ LibroCalificaciones_3/LibroCalificaciones_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LibroCalificaciones_3
+{
+    internal class PruebaLibroCalificaciones_3
+    {
+        static void Main(string[] args)
+        {
+            LibroCalificaciones MyLibro = new LibroCalificaciones("CS181 Programación en C#");
+            LibroCalificaciones MyLibro2 = new LibroCalificaciones("CS150 Estructuras de Datos");
+
+            Console.WriteLine("Bienvenido al Libro de Calificaciones para: {0}", MyLibro.NombreCurso);
+            Console.WriteLine("Bienvenido al Libro de Calificaciones para: {0}", MyLibro2.NombreCurso);
+            Console.WriteLine();
+
+            Console.WriteLine("Por favor ingrese el nombre del curso: ");
+            string nombreDelCurso = Console.ReadLine();
+
+            MyLibro.NombreCurso = nombreDelCurso;
+
+            Console.WriteLine();
+            Console.WriteLine("El nombre del Curso es: {0}", MyLibro.NombreCurso);
+            Console.WriteLine();
+
+            MyLibro.MostrarMensaje();
+
+            Console.WriteLine("\nPresione cualquier tecla para cerrar esta ventana...");
+            Console.ReadKey();
+        }
+    }
+}
